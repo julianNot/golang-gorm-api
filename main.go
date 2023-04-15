0f11b71692c5b5c64d68cfef04903ed577737adc
Julian Ardila <julian07099@gmail.com>
2023-04-14 23:49:04 -0500

    Added connection GORM ORM database

--- main.go
+++ main.go
@@ -4,10 +4,13 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/julianNot/golang-gorm-api/db"
 	"github.com/julianNot/golang-gorm-api/routes"
 )
 
 func main() {
+	db.DBConnection()
+	
 	router := mux.NewRouter()
 	router.HandleFunc("/", routes.HomeHandler)
 
